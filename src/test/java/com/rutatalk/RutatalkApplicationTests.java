f58c8f0a3b16b66fa24234c9d84e88e388b8642c
DESKTOP-JPP5GVA\Moon <foqlzm12345@gmail.com>
2023-05-18 17:52:47 +0900

    초기 환경 설정

---
+++ src/test/java/com/rutatalk/RutatalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutatalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutatalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
