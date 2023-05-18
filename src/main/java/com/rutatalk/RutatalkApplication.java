c770751a0800234198337e45d680bbc4e1eed742
DESKTOP-JPP5GVA\Moon <foqlzm12345@gmail.com>
2023-05-18 23:30:25 +0900

    environment setting

--- src/main/java/com/rutatalk/RutatalkApplication.java
+++ src/main/java/com/rutatalk/RutatalkApplication.java
@@ -1,10 +1,14 @@
 package com.rutatalk;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
-public class RutatalkApplication {
+public class RutatalkApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RutatalkApplication.class, args);
