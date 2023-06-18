f67ff3b40ce1f4ae1267e7bbd5b9f6133960b043
test <test@test.com>
2023-06-18 16:24:42 +0900

    회원가입 test 완료

--- src/main/java/com/rutatalk/user/entity/User.java
+++ src/main/java/com/rutatalk/user/entity/User.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -19,6 +21,7 @@ import lombok.Getter;
 @Table(name="user")
 public class User {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Column(name = "loginId", nullable = false)
