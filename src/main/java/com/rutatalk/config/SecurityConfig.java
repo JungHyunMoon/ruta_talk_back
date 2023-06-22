ccb4714e570b95373a00f48894ebc146f45f2c42
paulham <pauljungho.ham@gmail.com>
2023-06-22 15:42:23 +0900

    #9 -ing

--- src/main/java/com/rutatalk/config/SecurityConfig.java
+++ src/main/java/com/rutatalk/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.rutatalk.config;
 
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.RequiredTypes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,19 +12,30 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	private static final String[] PERMIT_ALL = {
+			"/",
+			"/css/**", "/img/**",
+			"/api/*/users/join",
+			"/api/*/users/login",
+			"/users/join",
+	};
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.cors().disable()			//cors 방지
-			.csrf().disable()			//csrf 방지
-			.formLogin().disable()		//기본 로그인페이지 없애기
-			.headers().frameOptions().disable();
- 
-		return http.build();
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.httpBasic().disable()
+				.csrf().disable()			//csrf 방지
+				.cors().and();
+
+		httpSecurity.authorizeHttpRequests(requests -> requests
+//				.requestMatchers(PERMIT_ALL).permitAll()
+				.anyRequest().permitAll()
+		);
+		return httpSecurity.getOrBuild();
 	}
 }
